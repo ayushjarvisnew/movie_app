461f3e90a894d02c7f2f143a1aad66bf0b2f87fd
Ayush Gupta <ayush.gupta@jarvis.consulting>
2025-11-06 12:14:54 +0530

    Fix signup CSRF for production

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 document.addEventListener("DOMContentLoaded", () => {
     // Set CSRF token for all Axios requests
+    axios.defaults.withCredentials = true;
     const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
     if (token) {
         axios.defaults.headers.common['X-CSRF-Token'] = token;
