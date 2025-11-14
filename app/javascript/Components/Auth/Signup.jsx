241e580e70c115e561e0b3d01a60b3efd3bb9aed
Ayush Gupta <ayush.gupta@jarvis.consulting>
2025-11-14 16:13:38 +0530

    remove localhosturl in signup

--- app/javascript/Components/Auth/Signup.jsx
+++ app/javascript/Components/Auth/Signup.jsx
@@ -43,7 +43,7 @@ const Signup = () => {
         if (!validate()) return;
 
         try {
-            const res = await axios.post("http://localhost:3000/signup", {
+            const res = await axios.post("/signup", {
                 name,
                 email,
                 password,
